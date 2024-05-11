4a8c40e8c9c7fa0531216fac3f8e83cc082870a2
MacDonald Mbizo <mbizomac@gmail.com>
2024-05-11 20:52:19 +0200

    create tables(extract,stage)

---
+++ create relavant extract and stage tables.sql
@@ -0,0 +1,13 @@
+-- CREATE TABLE extract.categories (
+--   CategoryID int ,
+--   CategoryName nvarchar(25) ,
+--   Description nvarchar(255) ,
+--   PRIMARY KEY (CategoryID)
+-- ) 
+
+CREATE TABLE stage.categories (
+   CategoryID int ,
+   CategoryName nvarchar(25) ,
+   Description nvarchar(255) ,
+   PRIMARY KEY (CategoryID)
+) 
