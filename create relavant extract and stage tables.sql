b3dfb5d67a36dab68bf1440d7db4333449b2340c
MacMbizo <92409325+MacMbizo@users.noreply.github.com>
2024-05-12 21:40:51 +0200

    created the rest of the extract and stage tables.

--- create relavant extract and stage tables.sql
+++ create relavant extract and stage tables.sql
@@ -1,13 +1,24 @@
--- CREATE TABLE extract.categories (
---   CategoryID int ,
---   CategoryName nvarchar(25) ,
---   Description nvarchar(255) ,
---   PRIMARY KEY (CategoryID)
--- ) 
+CREATE TABLE extract.suppliers (
+  SupplierID int ,
+  SupplierName nvarchar(50) ,
+  ContactName nvarchar(50) ,
+  Address nvarchar(50) ,
+  City nvarchar(20) ,
+  PostalCode nvarchar(10) ,
+  Country nvarchar(15) ,
+  Phone nvarchar(15) ,
+  PRIMARY KEY (SupplierID)
+)
 
-CREATE TABLE stage.categories (
-   CategoryID int ,
-   CategoryName nvarchar(25) ,
-   Description nvarchar(255) ,
-   PRIMARY KEY (CategoryID)
-) 
+
+CREATE TABLE stage.suppliers (
+  SupplierID int ,
+  SupplierName nvarchar(50) ,
+  ContactName nvarchar(50) ,
+  Address nvarchar(50) ,
+  City nvarchar(20) ,
+  PostalCode nvarchar(10) ,
+  Country nvarchar(15) ,
+  Phone nvarchar(15) ,
+  PRIMARY KEY (SupplierID)
+)
